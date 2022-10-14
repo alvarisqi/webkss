9b374004a5cce7a84ff667688a77d91e1874d044
alvarisqi <101438298+alvarisqi@users.noreply.github.com>
2022-10-14 23:32:24 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="assignment1.css">
+    <title>Assignment 1</title>
+</head>
+<body>
+    <!-- Nabvar -->
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Hactive8</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Profil</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="challenge2.html">Portofolio</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Tutorial
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">HTML</a></li>
+                  <li><a class="dropdown-item" href="#">CSS</a></li>
+                  <li><a class="dropdown-item" href="#">Javascript</a></li>
+                  <li><a class="dropdown-item" href="#">JQuery</a></li>
+                  <li><a class="dropdown-item" href="#">Codeigniter</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="d-flex" role="button">
+              <button class="btn" type="submit">Login</button>
+              <button class="btn bg-primary text-light" type="submit">Daftar</button>
+            </form>
+          </div>
+        </div>
+    </nav>
+</body>
+</html>
